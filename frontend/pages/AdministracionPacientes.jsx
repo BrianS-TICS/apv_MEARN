6916c150c7300d5b190f02df1460a7bf40517559
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-12-24 16:37:54 -0700

    Se agrega formulario condicional

--- frontend/pages/AdministracionPacientes.jsx
+++ frontend/pages/AdministracionPacientes.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ListadoPacientes from '../components/ListadoPacientes'
 import Formulario from '../components/Formulario'
 
+
 const AdministracionPacientes = () => {
+    const [mostarFormulario, setMostrarFormulario] = useState(false)
+    const handleMostrarFormulario = () => {
+        setMostrarFormulario(!mostarFormulario)
+    }
     return (
         <>
             <div className='flex flex-col md:flex-row gap-5 mt-5'>
-                <div className='md:w-1/2 lg:w-2/5'>
+                <button onClick={handleMostrarFormulario} type='button' className='py-2 px-2 bg-indigo-600 hover:bg-indigo-800 transition-colors md:hidden text-white font-bold rounded-md uppercase hover:cursor-pointer mx-5 mt-5'>
+                    {!mostarFormulario ? 'Mostrar formulario' : "Ocultar formulario"}
+                </button>
+                <div className={`${mostarFormulario ? 'block' : 'hidden'} md:block md:w-1/2 lg:w-2/5`}>
                     <Formulario />
                 </div>
                 <div className='md:w-1/2 lg:w-3/5'>
